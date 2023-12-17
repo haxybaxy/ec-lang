6ccbe14e1320161be718d06b322cde61156395c5
haxy <zaidsaheb4@gmail.com>
2023-12-17 22:32:41 +0100

    working final sebas do ur thang

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/zaidsaheb/desktop/ec-lang
+# Install script for directory: /Users/zaidsaheb/desktop/dist/ec-lang
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/zaidsaheb/desktop/ec-lang/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/zaidsaheb/desktop/dist/ec-lang/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
